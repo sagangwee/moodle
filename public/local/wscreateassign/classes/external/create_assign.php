c9d76ab5ecb1afce90358206728635fef9780e6a
Sagang <9004726+sagangwee@users.noreply.github.com>
2025-10-31 11:15:20 -0400

    Create assign missing course module ID

--- public/local/wscreateassign/classes/external/create_assign.php
+++ public/local/wscreateassign/classes/external/create_assign.php
@@ -1,13 +1,12 @@
 <?php
 namespace local_wscreateassign\external;
 
+use core\context;
 use core_external\external_api;
 use core_external\external_function_parameters;
 use core_external\external_multiple_structure;
 use core_external\external_single_structure;
 use core_external\external_value;
-
-use core\context_course;
 
 class create_assign extends external_api {
 
@@ -22,42 +21,34 @@ class create_assign extends external_api {
     }
 
     public static function execute($courseid, $name, $intro) {
-        // global $CFG, $DB;
+        global $CFG, $DB;
+        require_once("$CFG->dirroot/mod/assign/lib.php");
+        self::validate_parameters(self::execute_parameters(), ['courseid' => $courseid, 'name' => $name, 'intro' => $intro]);
 
-        // self::validate_parameters(self::execute_parameters(), ['courseid' => $courseid, 'name' => $name, 'intro' => $intro]);
+        $context = context\course::instance($courseid, IGNORE_MISSING);
+        self::validate_context($context);
 
-        // $context = \core\context_course::instance($courseid);
-        // self::validate_context($context);
+        $assignment = new \stdClass();
+        $assignment->course = $courseid;
+        $assignment->coursemodule = $courseid;
+        $assignment->name = $name;
+        $assignment->intro = $intro;
+        $assignment->introformat = FORMAT_HTML;
+        $assignment->assignsubmission_onlinetext_enabled = 1;
+        $assignment->assignsubmission_file_enabled = 1;
+        $assignment->grade = 100;
+        $assignment->timemodified = time();
+        $assignment->timeavailable = time();
+        $assignment->timedue = time() + 7 * 24 * 3600;
 
-        // require_once($CFG->dirroot . '/course/lib.php');
-        // require_once($CFG->dirroot . '/mod/assign/locallib.php');
-
-        // $assignment = new \stdClass();
-        // $assignment->course = $courseid;
-        // $assignment->name = $name;
-        // $assignment->intro = $intro;
-        // $assignment->introformat = FORMAT_HTML;
-        // $assignment->assignsubmission_onlinetext_enabled = 1;
-        // $assignment->assignsubmission_file_enabled = 1;
-        // $assignment->grade = 100;
-        // $assignment->timemodified = time();
-        // $assignment->timeavailable = time();
-        // $assignment->timedue = time() + 7 * 24 * 3600;
-
-        // $coursemodule = assign_add_instance($assignment, null);
+        $assignment_id = assign_add_instance($assignment, null);
 
         // $assignmentinstance = $DB->get_record('assign', ['id' => $coursemodule->instance]);
 
-        // return ['assignmentid' => $assignmentinstance->id, 'coursemoduleid' => $coursemodule->id];
-        return ['assignmentid' => 0, 'coursemoduleid' => 42];
+        return $assignment_id;
     }
 
     public static function execute_returns() {
-        return new external_single_structure(
-            [
-                'assignmentid' => new external_value(PARAM_INT, 'New assignment ID'),
-                'coursemoduleid' => new external_value(PARAM_INT, 'New course module ID'),
-            ]
-        );
+        return new external_value(PARAM_INT, 'New assignment ID');
     }
 }
